1b3656ffb21d77e473d56166aec3c86eca213b1c
marlydehaard <marlydehaard@gmail.com>
2025-02-26 16:29:04 +0100

    Project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "astro"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:astro/recommended",
+    "prettier",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off", // Not needed in Astro
+    "react/prop-types": "off", // Not necessary with TypeScript
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        "astro/no-set-html-directive": "warn",
+      },
+    },
+  ],
+};
---
+++ src/pages/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/global.css'
+
+export const App = () => {
+  return (
+    <main>
+      <h1>Welkom!</h1>
+    </main>
+  )
+}
