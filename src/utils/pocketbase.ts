6a2f202d02bb3c65fc461f493f86ced021b592cd
marlydehaard <marlydehaard@gmail.com>
2025-03-06 12:25:24 +0100

    fix: console log weggehaald

--- src/utils/pocketbase.ts
+++ src/utils/pocketbase.ts
@@ -1,7 +1,5 @@
 import PocketBase from 'pocketbase'
 
-console.log('PocketBase URL in Vercel:', import.meta.env.PUBLIC_POCKETBASE_URL)
-
 const pb = new PocketBase(import.meta.env.PUBLIC_POCKETBASE_URL)
 
 export default pb
