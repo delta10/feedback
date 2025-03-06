2df1c684ed40f63430e59fc910ab127a2f8d917f
marlydehaard <marlydehaard@gmail.com>
2025-03-06 12:24:04 +0100

    debug: vercel debug

--- src/utils/pocketbase.ts
+++ src/utils/pocketbase.ts
@@ -1,5 +1,7 @@
 import PocketBase from 'pocketbase'
 
+console.log('PocketBase URL in Vercel:', import.meta.env.PUBLIC_POCKETBASE_URL)
+
 const pb = new PocketBase(import.meta.env.PUBLIC_POCKETBASE_URL)
 
 export default pb
